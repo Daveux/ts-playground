1f85265b7eda6cad76639673f11e70712981707a
David Odeyinka <david.odeyinka@yahoo.com>
2020-06-16 01:20:59 +0100

    Working on controller to implement basic queries

--- src/controllers/book_controller.ts
+++ src/controllers/book_controller.ts
@@ -2,7 +2,16 @@ import { Request, Response} from "express";
 
 import  Book from "../models/books";
 
+export const allbooks = function(req: Request, res: Response) {
 Book.find({})
-    .then(function bookFound() {
-
+    .then(function bookFound(books: any) {
+        res.send(books);
     })
+    .catch(function bookNotFound(err: any) {
+        res.send(err);
+    })
+}
+
+export const 
+Book.findById()
+    .then()
